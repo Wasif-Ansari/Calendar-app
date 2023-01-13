5fd856d6163f51f13168fc5959c4467deef971f4
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-13 22:57:53 +0530

    placing widgets

--- calendar_gui.py
+++ calendar_gui.py
@@ -15,5 +15,11 @@ year_field=Entry(new)
 button = Button(new, text='Show Calender',
 fg='Black',bg='Blue')
 
+#putting widgets in position
+cal.place(x=120,y=10)
+year.place(x=165,y=70)
+year_field.place(x=130,y=100)
+button.place(x=145,y=140)
+
 
 new.mainloop()
