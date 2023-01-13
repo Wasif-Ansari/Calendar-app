31c8948b0e8b832a8d0b3fdc47e401f64ee66295
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-13 22:52:10 +0530

    creating labels and entry and button

--- calendar_gui.py
+++ calendar_gui.py
@@ -9,7 +9,11 @@ new = Tk()
 new.config(background='grey')
 new.title("Calendar")
 new.geometry("400x200")
-
+cal = Label(new, text="Calendar",bg='grey',font=("times", 28, "bold"))
+year = Label(new, text="Enter year", bg='dark grey')
+year_field=Entry(new)
+button = Button(new, text='Show Calender',
+fg='Black',bg='Blue')
 
 
 new.mainloop()
